9efe824070145aab71900a6ad28a6cfe561d21c4
Luke Russell <thelukerussel21@gmail.com>
2020-02-13 17:09:20 -0500

    started game_start page

--- game_start.py
+++ game_start.py
@@ -20,20 +20,48 @@ def script(str):
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.1)
-script(color.GREEN + color.BOLD + color.UNDERLINE + '\nWelcome to Rachel & Luke\'s Choose Your Own Adventure Game!' + color.END)
-script(color.BLUE + color.BOLD + '\nUsually, going to the grocery store is easy right?!' + color.END)
-script(color.RED + color.BOLD + '\nThink again          muahahaha    ' + color.END)
-script(color.YELLOW + color.BOLD + '\nPlayer 1....... are you ready? Y/N' + color.END)
+script(color.GREEN + color.BOLD + color.UNDERLINE + '\nWelcome to THE Choose Your Own Adventure Game!' + color.END)
+script(color.BLUE + color.BOLD + '\n\nUsually, going to the grocery store is easy right?!' + color.END)
+script(color.RED + color.BOLD + '\n\nThink again          muahahaha    ' + color.END)
+script(color.YELLOW + color.BOLD + '\n\nPlayer 1....... are you ready? Y/N' + color.END)
 player_ready = input(color.YELLOW + color.BOLD + '\n> ' + color.END)
 if player_ready.lower() == 'y':
     script(color.YELLOW + color.BOLD + """
-    Let's get started!
-    What's your name?
-    """ + color.END)
+Let's get started!
+What's your name?
+> """ + color.END)
+    player_name = input()
+    script(color.GREEN + color.BOLD + '\nNice to meet you ' + player_name + color.END)
+    script(color.RED + color.BOLD + '\nLET\'S DO THIS' + color.END)
+    script(color.GREEN + '\n\n\nYour goal is simple, go to a grocery store and get items off the list' + color.END)
+    script(color.GREEN + '\nHere\'s the kicker..   you have options' + color.END)
+    script(color.YELLOW + '\nWould you like to shop at Walmart or Whole Foods?\n> ')
+    store = input()
+    if store.lowercase() == 'walmart':
+        import walmart
+    elif store.lowercase() == 'whole foods':
+        import whole_foods
+    else:
+        print('You have to choose!')
 elif player_ready.lower() == 'n':
     script(color.RED + color.BOLD + """
-    TOO BAD
-    LET\'S GET 
-    STARTED!!
+TOO BAD
+
+LET'S
+     GET
+        STARTED!!
     """ + color.END)
-    print(color.YELLOW + color.BOLD + 'What is your name?' + color.END)
+    script(color.YELLOW + color.BOLD + 'What is your name?\n> ' + color.END)
+    player_name = input()
+    script(color.GREEN + color.BOLD + '\nNice to meet you ' + player_name + color.END)
+    script(color.RED + color.BOLD + '\nLET\'S DO THIS' + color.END)
+    script(color.GREEN + '\n\n\nYour goal is simple, go to a grocery store and get items off the list' + color.END)
+    script(color.GREEN + '\nHere\'s the kicker..   you have options' + color.END)
+    script(color.YELLOW + '\nWould you like to shop at Walmart or Whole Foods\n> ')
+    store = input()
+    if store == 'walmart':
+        import walmart
+    elif store == 'whole foods':
+        import whole_foods
+    else:
+        print('You have to choose!')
